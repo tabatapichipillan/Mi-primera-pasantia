b6b9e1a37c3ed0dfc2cf491ed67020d5b8e250f1
tabatapichipillan <pptpichipillan@zonar.cl>
2023-12-05 13:00:01 -0300

    index-contacto

--- index.js
+++ index.js
@@ -7,9 +7,15 @@ app.set('port', 3000)
 
 
 //routes
-app.get('/',(req,res)=>{
-    res.send('hola')
+
+app.get('/',(req,response)=>{
+    response.sendFile(path.resolve(__dirname, 'index.html'))
 })
+
+app.get('/contacto',(req,response)=>{
+    response.sendFile(path.resolve(__dirname, 'contacto.html'))
+})
+
 
 app.listen(app.get('port'), ()=>{
     console.log(`app corriendo en puerto ${app.get('port')}`)
